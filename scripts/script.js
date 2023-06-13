2a8ade9cb2853c3e85811a5eb14967005d871906
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-06-13 18:35:57 +0300

    Round logic
    Write basic functions of round logic.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 /*variables */
 
 const cpuChoisePool = ["","rock", "paper", "scissors"];
+const messagePool = ["Round is yours! ", "It'/s a tie. ", "Bad for you! You lost round. ", "Congratulations! You won the game", "Sorry, you/'ve lost."]
+const messageScore = "Your score is: " + playerScore + ". CPU score is: " + cpuScore + ".";
 let playerChoise;
 let cpuChoise;
 let roundResult;
+let playerScore = 0;
+let cpuScore = 0;
 let gameScore;
 
 /*players input*/
@@ -21,8 +25,33 @@ cpuChoise = cpuChoisePool[Math.floor(Math.random() * 3 + 1)];
 
 /*round calculation*/
 
-function round {
-    if (playerChoise === "rock" && cpuChoise ==="rock") {
+function tie() {
+    roundResult = messagePool[1] + messageScore;
+    console.log(roundResult);
+}
 
+function win() {
+    playerScore++;
+    roundResult = messagePool[0] + messageScore;
+    console.log(roundResult);
+}
+
+function lost() {
+    cpuScore++;
+    roundResult = messagePool[2] + messageScore;
+    console.log(roundResult);
+}
+
+function round() {
+    if (playerChoise === "rock") {
+        if (cpuChoise ==="rock") {
+            tie();
+        } else if (cpuChoise === "scissors") {
+            win();
+        } else if (cpuChoise === "paper") {
+            lost();
+        }
+    } else if (playerChoise === "paper") {
+        if
     }
 }
