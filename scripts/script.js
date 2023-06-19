d60e72d15e9e81a80638877329cf82ecdc7cfa9a
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-06-19 15:14:33 +0300

    Add bugs
    Add bunch of bugs will deal tomorrow

--- scripts/script.js
+++ scripts/script.js
@@ -8,25 +8,63 @@ let playerChoise;
 let cpuChoise;
 let roundResult;
 let roundCounter;
+const screen = document.getElementById('scrn-button');
+const cpuScreen = document.getElementById('cpu-screen')
 
 /*players input*/
 
 function getPlayerChoise() {
-//playerChoise = prompt("Your turn! Hit Rock, Paper Or Scissors:");
-playerChoise = playerChoise.toLowerCase();
-if (playerChoise !== "rock" && playerChoise !== "paper" && playerChoise !=="scissors") {
-    console.log("Wrong! You should promt rock, paper or scissors!");
-    getPlayerChoise();
-} else {
-    console.log("You hit " + playerChoise + ".")
-};
-}
+    screen.textContent = 'Choose your option:';
+    const optionButtons = document.querySelectorAll('button.option');
+    let playerChoice = null;
+  
+    optionButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        playerChoice = button.getAttribute('name');
+        screen.textContent =`You chose: ${playerChoice}`;
+      });
+    });
+  
+    return playerChoice;
+  }
+  
 
 /*cpu input*/
 
 function getComputerChoise() {
-cpuChoise = cpuChoisePool[Math.floor(Math.random() * 3 + 1)];
-console.log("CPU hits " + cpuChoise + ".")
+    const optionButtons = document.querySelectorAll('button.option');
+    let cpuChoiseChoice = null;
+    let cpuImage = null;
+    let cpuButton = null;
+    let cpuScreenDiv = document.getElementById('cpu-screen');
+    
+    optionButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            cpuChoise = cpuChoisePool[Math.floor(Math.random() * 3 + 1)];
+            switch (cpuChoise) {
+                case "rock":
+                    cpuButton = document.createElement('button');
+                    cpuButton.setAttribute('class', 'option');
+                    cpuImage = document.createElement ('i');
+                    cpuImage.setAttribute('class','fa-regular fa-hand-back-fist')
+                    cpuButton.appendChild(cpuImage);
+                    break;
+                case "paper":
+                    cpuButton = document.createElement('button');
+                    cpuButton.setAttribute('class', 'option');
+                    cpuImage = document.createElement ('i');
+                    cpuImage.setAttribute('class','ffa-regular fa-hand')
+                    cpuButton.appendChild(cpuImage);
+                case "scissors":
+                    cpuButton = document.createElement('button');
+                    cpuButton.setAttribute('class', 'option');
+                    cpuImage = document.createElement ('i');
+                    cpuImage.setAttribute('class','fa-regular fa-hand-scissors')
+                    cpuButton.appendChild(cpuImage);
+            }
+            cpuScreenDiv.appendChild(cpuButton);
+        });
+      });
 }
 
 /*round calculation*/
@@ -48,7 +86,9 @@ function lost() {
     console.log(roundResult);
 }
 
-function game(playerChoise, cpuChoise) {
+
+
+function round(playerChoise, cpuChoise) {
     switch (playerChoise) {
         case "rock":
             switch (cpuChoise) {
@@ -92,19 +132,20 @@ function game(playerChoise, cpuChoise) {
     }
 }
 
+screen.addEventListener('click', game);
+
+function game() {
 for (roundCounter = 0; roundCounter < 5; roundCounter++) {
     console.log("Round " + (roundCounter + 1) + "!");
     getPlayerChoise();
     getComputerChoise();
-    game ();
-}
-
-/* end game */
-
+    round();
+};
 if (playerScore > cpuScore) {
     console.log(messagePool[3]);
 } else if (playerScore < cpuScore) {
     console.log(messagePool[4]);
 } else {
     console.log(messagePool[5]);
+}
 }
